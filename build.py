36dbdc5b26b8639b246a3f13e8bb459a9159a3cc
Jasper Edbrooke <jasperredrock@gmail.com>
2021-08-31 15:39:41 -0700

    initial sub-page builder

--- build.py
+++ build.py
@@ -1,4 +1,3 @@
-from bs4.element import Tag
 import yaml
 import os
 import bs4
@@ -6,6 +5,46 @@ import copy
 
 ARTICLES_PATH = "articles"
 HEADER_PATH = "subpage_head.html"
+
+TAG_BADGE_CLASS = "badge bg-secondary text-decoration-none link-light"
+MAIN_TEXT_CLASS = "fs-5 mb-4"
+SUB_HEADER_CLASS = "fw-bolder mb-4 mt-5"
+
+def local_image(data):
+    tag = bs4.BeautifulSoup().new_tag("img")
+    if 'alt' in data:
+        tag['alt'] = data['alt']
+    if 'src' in data:
+        tag['src'] = f"/images/{data['src']}"
+    return tag
+
+def web_image(data):
+    tag = bs4.BeautifulSoup().new_tag("img")
+    if 'alt' in data:
+        tag['alt'] = data['alt']
+    if 'src' in data:
+        tag['src'] = data['src']
+    return tag
+
+def text(data):
+    tag = bs4.BeautifulSoup().new_tag("p")
+    tag['class'] = MAIN_TEXT_CLASS
+    tag.append(bs4.BeautifulSoup(data,"lxml"))
+    return tag
+
+def sub_header(data):
+    tag = bs4.BeautifulSoup().new_tag("h2")
+    tag['class'] = SUB_HEADER_CLASS
+    tag.append(bs4.BeautifulSoup(data,"lxml"))
+    return tag
+
+ITEM_ACTIONS = {
+    "local-image": local_image,
+    "text": text,
+    "web-image": web_image,
+    "sub-header": sub_header,
+}
+
 
 def main():
     with open(HEADER_PATH) as fh:
@@ -15,11 +54,30 @@ def main():
         with open(os.path.join(ARTICLES_PATH,path)) as article_yaml:
             data = yaml.safe_load(article_yaml)
             print(data)
-            html = copy.copy(page_template)
-            title_tag = page_template.new_tag("title")
-            title_tag.append(f"Jedbrooke - {data['title']}")
-            html.head.append(title_tag)
-            print(html.prettify())
+            if 'draft' in data and data['draft'] == True:
+                print("draft, skipping")
+            else:
+                html = copy.copy(page_template)
+                title_tag = page_template.new_tag("title")
+                title_tag.append(f"Jedbrooke - {data['title']}")
+                html.head.append(title_tag)
+                html.find(id="main-title").append(data['title'])
+                html.find(id="date").append(data['date'])
+
+                article_header = html.find(id="article-header")
+                for tag in data['tags']:
+                    badge = page_template.new_tag("a")
+                    badge.append(tag)
+                    badge['class'] = TAG_BADGE_CLASS
+                    badge['href'] = f"tags.html#{tag}"
+                    article_header.append(badge)
+                article_content = html.find(id="article-content")
+                for item in data['body']:
+                    for k,v in item.items():
+                        article_content.append(ITEM_ACTIONS[k](v))
+                
+                with open(os.path.join("public_html",path.replace(".yaml",".html")),"w") as ofh:
+                    ofh.write(html.prettify())
         print("------")
 
 
